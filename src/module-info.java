5100847daffc6469d7e417f03bc31f3c2cd615c5
eduardoaguiarc <eduardocamposaguiar57@gmail.com>
2026-08-19 15:19:35 -0300

    About View

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshop_javafx_jdbc {
     requires javafx.controls;
     requires java.sql;
 	requires javafx.fxml;
+	requires javafx.graphics;
 
     opens application to javafx.graphics, javafx.fxml;
     opens gui to javafx.fxml;
